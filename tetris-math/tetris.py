fb8d6b6fbdb34c6ed929cb5175016f90aed423ea
bevarly <24.sandyy@gmail.com>
2024-01-19 13:32:28 -0800

    clean up

--- tetris-math/tetris.py
+++ tetris-math/tetris.py
@@ -1,10 +1,6 @@
+import time
 import pygame
-
 from pygame.locals import(
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
@@ -12,17 +8,12 @@ from pygame.locals import(
 pygame.init()
 
 screen = pygame.display.set_mode([450, 700])
-
 x = 200
 y = 1
-
 width = 20
 height = 20
-
 vel = 15
-
 screen.fill((159, 226, 191))
-
 color = (114, 79, 128)
 
 running = True
@@ -31,10 +22,8 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-
         elif event.type == QUIT:
             running = False
-
         keys = pygame.key.get_pressed()
         if keys[pygame.K_DOWN] and y>0:
             color = (159, 226, 191)
@@ -43,32 +32,24 @@ while running:
             color = (114, 79, 128)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
             keys_not = pygame.K_END
-
         if keys[pygame.K_UP] and y>0:
             color = (159, 226, 191)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
             y-= vel
             color = (114, 79, 128)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
-
         if keys[pygame.K_LEFT] and x>0:
             color = (159, 226, 191)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
             x-= vel
             color = (114, 79, 128)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
-
         if keys[pygame.K_RIGHT] and x>0:
             color = (159, 226, 191)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
             x+= vel
             color = (114, 79, 128)
             pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
-
     pygame.draw.rect(screen, color, pygame.Rect(x, y, width, height))
-
     pygame.display.flip()
-
 pygame.quit()
-
-#next task: make object keep moving as key is pressed down.
