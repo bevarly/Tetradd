6435dc3568bc9e089f0cc8244bd66780ad97f181
bevarly <24.sandyy@gmail.com>
2024-04-30 13:15:29 -0700

    finalized the equation and user input to allow movement

--- src/tetris.py
+++ src/tetris.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from pygame.locals import(
+from pygame.locals import (
     K_ESCAPE,
     K_DOWN,
     K_LEFT,
@@ -22,111 +22,88 @@ class Tetradd:
         self.user_text = ''
         self.equation = ''
         self.str_sum = ''
-        self.width = 20
-        self.height = 20
         self.screenColor = (159, 226, 191)
-        self.rectColor = (114, 79, 169)
         self.color_on = (237, 199, 250)
         self.color_off = (0, 0, 0)
-        #self.num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.num1 = random.choice(num_list)
-        self.num2 = random.choice(num_list)
-        self.equation = str(num1) + "+" + str(num2)
-        self.sum = num1 + num2
-        self.str_sum = str(sum)
+        self.num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.num1 = random.choice(self.num_list)
+        self.num2 = random.choice(self.num_list)
+        self.equation = str(self.num1) + "+" + str(self.num2)
+        self.sum = self.num1 + self.num2
+        self.str_sum = str(self.sum)
         self.active = False
         self.x = 210
-        self.y = 120 #starting blocks height
+        self.y = 120  # starting blocks height
         self.CONSTANT_VEL = 5
-        #addition
-        self.rotation_angle = 0 #initial rotation angle
-        self.rotation_speed = 0.5
-    def event_list:
-        running = True
-        while running:
-            clock.tick(100)
+        # addition
+        self.rotation_angle = 0  # initial rotation angle
+        self.user_correct = False
+        self.running = True
+
+    def event_list(self):
+        while self.running:
+            self.clock.tick(60)
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    running = False
+                    self.running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if answer_box.collidepoint(event.pos):
-                        active = True
+                    if self.answer_box.collidepoint(event.pos):
+                        self.active = True
                     else:
-                        active = False
+                        self.active = False
                 if event.type == pygame.KEYDOWN:
-                    if active == True:
+                    if self.active:
                         if event.key == pygame.K_BACKSPACE:
-                            user_text = user_text[0:-1]
+                            self.user_text = self.user_text[0:-1]
                         else:
-                            user_text += event.unicode
+                            self.user_text += event.unicode
 
-    y += 0
-    user_correct = False
-    if str_sum == user_text:
-        y += 1
-        user_correct = True
-        keys = pygame.key.get_pressed()
-        if keys[K_ESCAPE]:
-            running = False
-            break
-        if keys[K_DOWN] and y < 650:
-            y += CONSTANT_VEL
-        if keys[K_LEFT] and x > 0:
-            x -= CONSTANT_VEL
-        if keys[K_RIGHT] and x < 430:
-            x += CONSTANT_VEL
-        if keys[K_SPACE]:
-            rotation_angle += math.pi / 2 # Rotate by 90 degrees
-        if user_correct == True and y > 650: #after each correct answer -
-            user_text = ''
-            rotation_angle = 0
-            x = 210 #setting x and y for each correct answer
-            y = 120
-            num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            num1 = random.choice(num_list)
-            num2 = random.choice(num_list)
-            equation = str(num1) + "+" + str(num2)
-            sum = num1 + num2
-            str_sum = str(sum)
+            if self.str_sum == self.user_text:
+                self.y += 1
+                self.user_correct = True
+                keys = pygame.key.get_pressed()
+                if keys[K_ESCAPE]:
+                    self.running = False
+                    self.quit()
+                if keys[K_DOWN] and self.y < 650:
+                    self.y += self.CONSTANT_VEL
+                if keys[K_LEFT] and self.x > 0:
+                    self.x -= self.CONSTANT_VEL
+                if keys[K_RIGHT] and self.x < 430:
+                    self.x += self.CONSTANT_VEL
+                if keys[K_SPACE]:
+                    self.rotation_angle += math.pi / 2  # Rotate by 90 degrees
+                if self.user_correct and self.y > 650:  # after each correct answer
+                    self.user_text = ""
+                    self.rotation_angle = 0
+                    self.x = 210  # setting x and y for each correct answer
+                    self.y = 120
+                    self.num1 = random.choice(self.num_list)
+                    self.num2 = random.choice(self.num_list)
+                    self.equation = str(self.num1) + "+" + str(self.num2)
+                    self.sum = self.num1 + self.num2
+                    self.str_sum = str(self.sum)
 
-    screen.fill(screenColor)
-    border = pygame.draw.rect(screen, (114, 79, 169), pygame.Rect(0, 0, 450, 120))
-    text_surface = base_font.render(game_title, True, (255, 255, 255))
-    screen.blit(text_surface, (173, 10))
+            self.screen.fill(self.screenColor)
+            pygame.draw.rect(self.screen, (114, 79, 169), pygame.Rect(0, 0, 450, 120))
+            self.screen.blit(self.base_font.render(self.game_title, True, (255, 255, 255)), (173, 10))
 
-    if active:
-        color = color_on
-    else:
-        color = color_off
-    input_box = pygame.draw.rect(screen, color, answer_box, 2)
-    user_answer = base_font.render(user_text, True, (40, 253, 21))
-    screen.blit(user_answer, (input_box.x + 5, input_box.y + 5))
-    answer_box.w = user_answer.get_width() + 30
+            color = self.color_on if self.active else self.color_off
+            pygame.draw.rect(self.screen, color, self.answer_box, 2)
+            user_answer = self.base_font.render(self.user_text, True, (40, 253, 21))
+            self.screen.blit(user_answer, (self.answer_box.x + 5, self.answer_box.y + 5))
+            self.answer_box.w = user_answer.get_width() + 30
 
-    equation_box = pygame.draw.rect(screen, color_on, equation_rect, 2)
-    equation_text = base_font.render(equation, True, (255, 255, 255))
-    screen.blit(equation_text, (equation_box.x + 5, equation_box.y + 5))
+            pygame.draw.rect(self.screen, self.color_on, self.equation_rect, 2)
+            equation_text = self.base_font.render(self.equation, True, (255, 255, 255))
+            self.screen.blit(equation_text, (self.equation_rect.x + 5, self.equation_rect.y + 5))
+            pygame.display.flip()
 
-    # L shape
-    rotated_polygon = []
-    for point in [(x, y), (x, y + 50), (x + 35, y + 50), (x + 35, y + 35), (x + 15, y + 35), (x + 15, y)]:
-        rotated_point = (
-            (point[0] - x) * math.cos(rotation_angle) - (point[1] - y) * math.sin(rotation_angle) + x,
-            (point[0] - x) * math.sin(rotation_angle) + (point[1] - y) * math.cos(rotation_angle) + y
-        )
-        rotated_polygon.append(rotated_point)
-    # square shape
-    rotated_polygon_2 = []
-    for point in [(x, y), (x, y + 50), (x + 50, y + 50), (x + 50, y + 50), (x + 50, y + 50), (x + 50, y)]:
-        rotated_point = (
-            (point[0] - x) * math.cos(rotation_angle) - (point[1] - y) * math.sin(rotation_angle) + x,
-            (point[0] - x) * math.sin(rotation_angle) + (point[1] - y) * math.cos(rotation_angle) + y
-        )
-        rotated_polygon_2.append(rotated_point)
+    def quit(self):
+        self.running = False
+        pygame.quit()
 
+if __name__ == '__main__':
+    t1 = Tetradd()
+    t1.event_list()
 
-    moving_blocks = pygame.draw.polygon(screen, (255, 255, 255), rotated_polygon)
-    moving_blocks = pygame.draw.polygon(screen, (255, 255, 255), rotated_polygon_2)
-
-    pygame.display.flip()
-pygame.quit()
