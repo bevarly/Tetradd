b709a32ba7f9ad166e2d0cc34b55a80b7aae12b3
bevarly <24.sandyy@gmail.com>
2024-05-07 23:26:40 -0700

    Update tetris.py

--- src/tetris.py
+++ src/tetris.py
@@ -9,7 +9,6 @@ from pygame.locals import (
     K_SPACE
 )
 import math
-import time
 
 
 class Rects:
@@ -36,8 +35,9 @@ class Rects:
         self.movement = 275
         self.count_list = [0, 1, 2, 3]
         self.counter = 0
+        self.SHAPES = []
 
-    def screenfill(self):
+    def screen_fill(self):
         self.screen.fill(self.colors)
         pygame.draw.rect(self.screen, (114, 79, 169), pygame.Rect(0, 0, 450, 120))
         self.screen.blit(self.fonts.render('Tetradd', True, (255, 255, 255)), (173, 10))
@@ -69,44 +69,48 @@ class Rects:
             pygame.display.flip()
             pygame.time.delay(0)
 
+    # def updatePos(self):
+
     def square_rect(self):
-        if (self.counter == 0):
-            #square shape
-            square = pygame.Rect((self.x - 2, self.y, 44, 44))
-            pygame.draw.rect(self.screen, (0, 0, 0), square)
-            pygame.draw.rect(self.screen, (253, 216, 53), (self.x, self.y + 2, 40, 40))
+        #square shape
+        square = pygame.Rect((self.x - 2, self.y, 44, 44))
+        pygame.draw.rect(self.screen, (0, 0, 0), square)
+        pygame.draw.rect(self.screen, (253, 216, 53), (self.x, self.y + 2, 40, 40))
 
+    def l_shape(self):
+        #L shape
+        pygame.draw.polygon(self.screen, (0, 0, 0),
+                            ([self.x, self.y], [self.x, self.y + 43], [self.x + 35, self.y + 43],
+                             [self.x + 35, self.y + 28], [self.x + 13, self.y + 28], [self.x + 13, self.y]))
 
+        pygame.draw.polygon(self.screen, (54, 103, 244),
+                            ([self.x + 2, self.y + 2], [self.x + 2, self.y + 41], [self.x + 33, self.y + 41],
+                             [self.x + 33, self.y + 30], [self.x + 11, self.y + 30], [self.x + 11, self.y + 2]))
 
+    def t_shape(self):
+        #T shape
+        pygame.draw.polygon(self.screen, (0, 0, 0), ([self.x - 7, self.y], [self.x - 7, self.y + 13],
+                                                     [self.x + 10, self.y + 13], [self.x + 10, self.y + 40],
+                                                     [self.x + 23, self.y + 40],
+                                                     [self.x + 23, self.y + 13], [self.x + 40, self.y + 13],
+                                                     [self.x + 40, self.y]))
+        pygame.draw.polygon(self.screen, (223, 29, 29), ([self.x - 5, self.y + 2], [self.x - 5, self.y + 10],
+                                                         [self.x + 12, self.y + 10], [self.x + 12, self.y + 38],
+                                                         [self.x + 21, self.y + 38],
+                                                         [self.x + 21, self.y + 10], [self.x + 38, self.y + 10],
+                                                         [self.x + 38, self.y + 2]))
 
-        elif self.counter == 1:
-            #L shape
-            pygame.draw.polygon(self.screen, (0, 0, 0),
-                                ([self.x, self.y], [self.x, self.y + 43], [self.x + 35, self.y + 43],
-                                 [self.x + 35, self.y + 28], [self.x + 13, self.y + 28], [self.x + 13, self.y]))
-
-            pygame.draw.polygon(self.screen, (54, 103, 244),
-                                ([self.x + 2, self.y + 2], [self.x + 2, self.y + 41], [self.x + 33, self.y + 41],
-                                 [self.x + 33, self.y + 30], [self.x + 11, self.y + 30], [self.x + 11, self.y + 2]))
-        elif self.counter == 2:
-            #T shape
-            pygame.draw.polygon(self.screen, (0, 0, 0), ([self.x-7, self.y], [self.x-7, self.y+13],
-                                                         [self.x+10, self.y+13], [self.x+10, self.y+35], [self.x+23, self.y+35],
-                                                         [self.x+23, self.y+13], [self.x+40, self.y+13], [self.x+40, self.y]))
-            pygame.draw.polygon(self.screen, (223, 29, 29), ([self.x-5, self.y+2], [self.x-5, self.y+10],
-                                                             [self.x+12, self.y+10], [self.x+12, self.y+32], [self.x+21, self.y+32],
-                                                         [self.x+21, self.y+10], [self.x+38, self.y+10], [self.x+38, self.y+2]))
-
-        elif self.counter == 3:
-            #I shape
-            pygame.draw.polygon(self.screen, (0, 0, 0),
-                                ([self.x-3, self.y], [self.x-3, self.y+50], [self.x+10, self.y+50], [self.x+10, self.y]))
-            pygame.draw.polygon(self.screen, (225, 12, 143),
-                                ([self.x-1, self.y+2], [self.x-1, self.y + 48], [self.x + 8, self.y + 48], [self.x + 8, self.y+2]))
-
+    def line_shape(self):
+        #I shape
+        pygame.draw.polygon(self.screen, (0, 0, 0),
+                            ([self.x - 3, self.y], [self.x - 3, self.y + 50], [self.x + 10, self.y + 50],
+                             [self.x + 10, self.y]))
+        pygame.draw.polygon(self.screen, (225, 12, 143),
+                            ([self.x - 1, self.y + 2], [self.x - 1, self.y + 48], [self.x + 8, self.y + 48],
+                             [self.x + 8, self.y + 2]))
 
     def mascot_rect(self):  #dog goes into abyss, work in progress
-        if self.user_ans != "":
+        if window.active:
             doggy = pygame.draw.rect(self.screen, (114, 79, 169), (self.movement, 40, 80, 80))
             image = pygame.image.load('pixel-art-cute-fox-png-t3atin7a90wibunw.png')
             image = pygame.transform.scale(image, (doggy.height, doggy.width))
@@ -121,6 +125,9 @@ class Nums(Rects):
         window.question_string = str(window.num1) + "+" + str(window.num2)
         window.answer = window.num1 + window.num2
         window.str_sum = str(window.answer)
+
+
+# class Shapes(pygame.sprite.Sprite):
 
 
 if __name__ == '__main__':
@@ -146,18 +153,18 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if window.active:
                     if event.key == pygame.K_BACKSPACE:
-                        window.user_ans = window.user_ans[0:-1]  #allow to delete
+                        window.user_ans = window.user_ans[0:-1]  #allows to delete
                     else:
-                        window.user_ans += event.unicode  #typ
-        window.screenfill()
+                        window.user_ans += event.unicode  #typing method
+        window.screen_fill()
         window.answer_rect()
-        window.square_rect()
+        # window.square_rect()
         window.mascot_rect()
+        # shape spawn
         if window.user_ans == window.str_sum:
             if window.y == 120:
                 window.correct_pop()
             window.y += 1
-            window.movement += 1
             user_correct = True
             keys = pygame.key.get_pressed()
             if keys[K_ESCAPE]:
@@ -173,10 +180,7 @@ if __name__ == '__main__':
             if user_correct and window.y >= 650:
                 window.user_ans = ""
                 rotation_angle = 0
-                window.x = 210
-                window.y = 120
                 rando.prob_nums()
-                window.counter = random.choice(window.count_list)
-                window.num_rect()
+
         window.num_rect()
         pygame.display.flip()
